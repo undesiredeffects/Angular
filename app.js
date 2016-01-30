9bf6f7b727811775e6e56219df97f978b030ac82
Christopher Jones <cejone08@louisville.edu>
2016-01-30 13:56:02 -0500

    1.7

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-
-
-(function(){
-  var gem = { name: 'Azurite', price: 2.95 };
+(function() {
   var app = angular.module('gemStore', []);
-app.controller('StoreController', function(){
- this.product = gem;
-});
+
+  app.controller('StoreController', function(){
+this.products = gems;
+  });
+
+  var gems = [
+    { name: 'Azurite', price: 2.95 },
+    { name: 'Bloodstone', price: 5.95 },
+    { name: 'Zircon', price: 3.95 }
+  ];
 })();
-
-
-	var gem = {
-		name : 'Dodecahedron',
-		price: 2.95,
-		description:'.....',
-		canPurchase: false;
-	}
-})();
-
-
-
