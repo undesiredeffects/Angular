3c1ff337b4dccc24129cd983dd86116c6cf986d5
Christopher Jones <cejone08@louisville.edu>
2016-01-30 20:38:05 -0500

    final

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 (function() {
   var app = angular.module('gemStore', ['store-directives']);
 
-app.controller('StoreController', ['$http', function($http){
-  var store = this;
-  store.products = [];
-
-  $http.get('/store-products.json').success(function(data){
-    store.products = data;
+  app.controller('StoreController', function(){
+    var store = this;
+    store.products = [];
+    
   });
-}]);
 
   app.controller('ReviewController', function() {
     this.review = {};
